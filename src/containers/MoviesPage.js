0c1d03dd981f4702a5702df040953abc5837530d
sally al sardouk <sally.sardouk98@gmail.com>
2021-02-07 08:53:31 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -7,9 +7,8 @@ import MovieShow from './MovieShow';
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
-     <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> }/>
-  </div>
+  </div>;
+
 )
- 
 
 export default MoviesPage
