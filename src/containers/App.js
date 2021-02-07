fcfe2ccb5be73614f3ba0d2a8785f05082a842fa
sally al sardouk <sally.sardouk98@gmail.com>
2021-02-07 08:53:08 +0000

    Automatically backed up by Learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,32 +1,14 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import MoviesPage from './MoviesPage';
+// .src/containers/MoviesPage.js
+import React from 'react';
+import { Route } from 'react-router-dom';
+import MoviesList from '../components/MoviesList';
+import MovieShow from './MovieShow';
 
-class App extends Component {
+const MoviesPage = ({ match, movies }) => (
+  <div>
+    <MoviesList movies={movies} />
+  </div>;
 
-  state = {
-    movies: {
-      1: { id: 1, title: 'A River Runs Through It' },
-      2: { id: 2, title: 'Se7en' },
-      3: { id: 3, title: 'Inception' }
-    }
-  }
+)
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <NavBar />
-          <Route exact path="/" render={() => <div>Home</div>} />
-          <Route path='/movies' render={routerProps => <MoviesPage {...routerProps} movies={this.state.movies}/>} />
-        </div>
-      </Router>
-    );
-  }
-}
-
-export default App;
+export default MoviesPage
